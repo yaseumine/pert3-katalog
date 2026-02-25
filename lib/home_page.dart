ce04f968e442a8979c2ec4a513d26704e2e9b876
yaseumine <yasminmaharani123@gmail.com>
2026-02-26 01:11:28 +0700

    ubah text menjadi angka.tostring

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 const Text('You have pushed the button this many times:'),
                 Text(
-                  '$_counter',
+                  value.angka.toString(),
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
               ],
