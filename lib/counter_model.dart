8b3c444fd6b89cb3ab6a00e39fd9cfc2dccd4ff5
yaseumine <yasminmaharani123@gmail.com>
2026-02-26 00:54:10 +0700

    update extends changenotifier dibutuhkan import material

--- lib/counter_model.dart
+++ lib/counter_model.dart
@@ -1,9 +1,12 @@
-class CounterModel {
+import 'package:flutter/material.dart';
+
+class CounterModel extends ChangeNotifier {
   int _angka = 0;
 
   int get angka => _angka;
 
   void increment() {
     _angka++;
+    notifyListeners(); // digunakan untuk memberitahu UI agar update
   }
 }
