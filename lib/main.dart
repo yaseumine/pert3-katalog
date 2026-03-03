805b32a7100257bcf0685db16bf816c3037956ba
yaseumine <yasminmaharani123@gmail.com>
2026-03-03 22:02:01 +0700

    menambahkan kode my_cart'

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( [cite: 174]
+    return MaterialApp(
       debugShowCheckedModeBanner: false, 
       initialRoute: '/', 
       routes: {
--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class CartModel extends ChangeNotifier {
-  // [cite: 138]
   final List<String> _items = [];
 
   List<String> get items => _items;
