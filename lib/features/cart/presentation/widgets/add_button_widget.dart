6a51aaaa05dd0c2a415acc83ed3f2eccde3c5cc7
yaseumine <yasminmaharani123@gmail.com>
2026-03-03 23:12:17 +0700

    perbaikan wodget button

--- lib/features/cart/presentation/widgets/add_button_widget.dart
+++ lib/features/cart/presentation/widgets/add_button_widget.dart
@@ -1,49 +1,28 @@
+// features/cart/presentation/widgets/add_button_widget.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../domain/entities/product.dart';
 import '../providers/cart_provider.dart';
 
-class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+class AddButtonWidget extends StatelessWidget {
+  final Product product;
+  const AddButtonWidget({required this.product, super.key});
 
   @override
   Widget build(BuildContext context) {
-    var cartProvider = context.watch<CartProvider>();
+    // context.select kini memantau CartProvider dan memeriksa ID produk
+    final isInCart = context.select<CartProvider, bool>(
+      (provider) => provider.isInCart(product.id),
+    );
 
-    return Scaffold(
-      appBar: AppBar(title: const Text('Keranjang Belanja')),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: cartProvider.items.length,
-              itemBuilder: (context, index) {
-                final Product item = cartProvider.items[index];
-
-                return ListTile(
-                  leading: const Icon(Icons.fastfood),
-                  title: Text(item.name),
-                  subtitle: Text('Rp ${item.price}'),
-                );
-              },
-            ),
-          ),
-
-          const Divider(),
-
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: ElevatedButton(
-              onPressed: () => cartProvider.removeAll(),
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-              child: const Text(
-                'Hapus Keranjang',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ),
-        ],
-      ),
+    return TextButton(
+      onPressed: isInCart
+          ? null
+          // context.read memanggil addItem di CartProvider dengan Product entity
+          : () => context.read<CartProvider>().addItem(product),
+      child: isInCart
+          ? const Icon(Icons.check, color: Colors.green)
+          : const Text('TAMBAH'),
     );
   }
 }
