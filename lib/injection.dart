2d2035a76aafd16ed5a01df71d97648aed95269d
yaseumine <yasminmaharani123@gmail.com>
2026-03-03 23:16:48 +0700

    perbaikan error injection

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'features/cart/data/domain/repositories/cart_repository_impl.dart';
-import 'features/cart/data/presentation/providers/cart_providers.dart';
+
+import 'features/cart/domain/repositories/cart_repository_impl.dart';
+import 'features/cart/presentation/providers/cart_provider.dart';
 import 'main.dart'; // Asumsi MyApp ada di main.dart, atau pindahkan MyApp ke sini jika perlu
 
 Widget buildApp() {
