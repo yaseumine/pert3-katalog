265d7ac40b9febe225cd0c18fb21e8b7532196da
yaseumine <yasminmaharani123@gmail.com>
2026-03-03 22:54:38 +0700

    menambahkan app router .dart

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../features/cart/data/presentation/pages/cart_pages.dart';
+import '../../features/cart/data/presentation/pages/catalog_page.dart';
+
+class AppRouter {
+  static const String catalog = '/';
+  static const String cart = '/cart';
+
+  static Map<String, WidgetBuilder> get routes => {
+    catalog: (context) => const CatalogPage(),
+    cart: (context) => const CartPage(),
+  };
+}
